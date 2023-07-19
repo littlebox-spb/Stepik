2d395c66154d461d9f2fe53f7e94e28d9dbc9e67
45 <45@littlebox.spb.ru>
2023-07-19 18:50:26 +0300

    modified:   init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
 
