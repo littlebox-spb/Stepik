acef5a1eaf64631779a82451844061c06698fd75
Littlebox <sasha@littlebox.spb.ru>
2023-07-19 17:40:14 +0300

    modified:   init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/Hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 Hello:wsgi_first
+
+
+#sudo gunicorn -b 0.0.0.0:8080 Hello:wsgi_first
 
 #sudo /etc/init.d/mysql start
