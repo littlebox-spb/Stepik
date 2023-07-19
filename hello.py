a59f7118530964d9968b160184d78680ebb895ed
45 <45@littlebox.spb.ru>
2023-07-19 19:08:04 +0300

    new file:   hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+from urllib.parse import parse_qs
+
+
+def wsgi_first(environ, start_response):
+    body = parse_qs(environ[QUERY_STRING])
+    status = "200 OK"
+    headers = [("Content-Type", "text/plain")]
+    start_response(status, headers)
+    return body
