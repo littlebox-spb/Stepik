5c085f658e88b9f5a324c26691175257038f5103
45 <45@littlebox.spb.ru>
2023-07-19 17:35:30 +0300

    modified:   Hello.py 	modified:   init.sh

--- Hello.py
+++ Hello.py
@@ -1,15 +1,6 @@
-from urllib.parse import urlparse, parse_qs
-
-
 def wsgi_first(environ, start_response):
-    body = []
+    body = "\n".join(environ[QUERY_STRING]).split("&")
     status = "200 OK"
     headers = [("Content-Type", "text/plain")]
     start_response(status, headers)
     return body
-
-
-s = "https://stepik.org/lesson/14826/step/11?unit=4175"
-
-print(urlparse(s))
-print(parse_qs(s[2:]))
--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/Hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 Hello:wsgi_first
+
 #sudo /etc/init.d/mysql start
