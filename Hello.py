82967010ee0e71c391763f737c37c4df2d0027ac
45 <45@littlebox.spb.ru>
2023-07-19 19:06:51 +0300

    deleted:    Hello.py

--- Hello.py
+++
@@ -1,9 +0,0 @@
-from urllib.parse import parse_qs
-
-
-def wsgi_first(environ, start_response):
-    body = parse_qs(environ[QUERY_STRING])
-    status = "2000 OK"
-    headers = [("Content-Type", "text/plain")]
-    start_response(status, headers)
-    return body
