72b1a02afcb023d2f4f352457ceea703f34cd836
Littlebox <sasha@littlebox.spb.ru>
2023-07-20 11:02:41 +0300

    deleted:    .mypy.ini 	deleted:    .vscode/settings.json 	deleted:    ask/ask/__init__.py 	deleted:    ask/ask/__pycache__/__init__.cpython-311.pyc 	deleted:    ask/ask/__pycache__/settings.cpython-311.pyc 	deleted:    ask/ask/asgi.py 	deleted:    ask/ask/settings.py 	deleted:    ask/ask/urls.py 	deleted:    ask/ask/wsgi.py 	deleted:    ask/manage.py 	deleted:    ask/qa/__init__.py 	deleted:    ask/qa/admin.py 	deleted:    ask/qa/apps.py 	deleted:    ask/qa/migrations/__init__.py 	deleted:    ask/qa/models.py 	deleted:    ask/qa/tests.py 	deleted:    ask/qa/views.py 	deleted:    etc/nginx.conf 	deleted:    hello.py 	deleted:    init.sh 	deleted:    public/css/read.me.txt 	deleted:    public/img/read.me.txt 	deleted:    public/js/read.me.txt 	deleted:    uploads/read.me.txt

--- hello.py
+++
@@ -1,6 +0,0 @@
-def wsgi_first(environ, start_response):
-    body = [bytes(i + "\n", "ascii") for i in environ["QUERY_STRING"].split("&")]
-    status = "200 OK"
-    headers = [("Content-Type", "text/plain")]
-    start_response(status, headers)
-    return body
--- init.sh
+++
@@ -1,8 +0,0 @@
-sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/hello.py   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_first
-
-#sudo /etc/init.d/mysql start
