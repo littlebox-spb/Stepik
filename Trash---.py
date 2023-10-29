fad9c0200c52571dcbc1bf05fd6c7630a5c1b961
Littlebox <sasha@littlebox.spb.ru>
2023-10-29 13:45:09 +0300

    Dataclasses.py 	MyRange.py

--- Trash---.py
+++ Trash---.py
@@ -1,4 +1,12 @@
-if type(5) == "int":
-    print("yes")
-else:
-    print("no")
+from contextlib import contextmanager
+
+
+@contextmanager
+def my_context_manager():
+    print("Начало контекстного менеджера ...")
+    yield "Ух ты как круто!"
+    print("Конец контекстного менеджера...")
+
+
+with my_context_manager() as phrase:
+    print(phrase)
