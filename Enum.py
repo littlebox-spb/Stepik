b096751c4ea106d16f8125dea5cd9ff3e882fb1f
Littlebox <sasha@littlebox.spb.ru>
2023-10-29 14:15:38 +0300

    new file:   Enum.py

---
+++ Enum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Size(Enum):
+    S = "small"
+    M = "medium"
+    L = "large"
+    XL = "extra large"
+    XXL = "extra extra large"
+
+
+# print(Direction["WEST"].name, Direction["SOUTH"].value, sep="\n")
