c9ba35822b288384421603e7212954ab80673f54
Littlebox <sasha@littlebox.spb.ru>
2023-11-19 01:19:38 +0300

    modified:   pop16.py

--- pop16.py
+++ pop16.py
@@ -1,3 +1,34 @@
+grafclass = {}
+
+
+def islinkclass(pr, pot):
+    if pr == pot or grafclass[pot].count(pr) != 0:
+        return True
+    else:
+        for i in grafclass[pot]:
+            if islinkclass(pr, i):
+                return True
+        return False
+
+
+for _ in range(int(input())):
+    k = input().split()
+    grafclass[k[0]] = []
+    cl = 2
+    while cl < len(k):
+        grafclass[k[0]].append(k[cl])
+        cl += 1
+
+for _ in range(int(input())):
+    k = list(input().split())
+    if islinkclass(k[0], k[1]):
+        print("Yes")
+    else:
+        print("No")
+
+# print(grafclass)
+
+
 # import time
 
 
