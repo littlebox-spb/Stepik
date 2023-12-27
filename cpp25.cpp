5b813d626f8402b0025cd7b3787187212e7eea34
Alexander Altukhov <sasha@littlebox.spb.ru>
2023-12-27 09:15:17 +0300

    modified:   cpp25.cpp 	modified:   main/hgyoc/ilwwv/psxra.txt

--- cpp25.cpp
+++ cpp25.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Learning C++ is easy!";
+    cout << "Learning C++ is easy!\n";
     setlocale(LC_ALL, "rus"); // подключение русскоязычного вывода
     cout << "Изучать С++ легко!";
     return 0;
