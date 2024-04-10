3c922c03358d4205626a920faf3a4fadb5931ca4
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-04-10 13:21:57 +0300

    modified:   cpp631.cpp 	modified:   cpp631.exe 	modified:   lessons.c 	modified:   lessons.exe

--- cpp631.cpp
+++ cpp631.cpp
@@ -1,36 +1,95 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main()
 {
-    int n;
-    std::cin >> n;
+    int n, m, s = 0;
+    std::cin >> n >> m;
     std::vector<int> arr;
     for (int i = 0; i < n; i++)
     {
-        int k;
-        std::cin >> k;
-        if (arr.size() == 0)
+        for (int j = 0; j < m; j++)
         {
+            int k;
+            std::cin >> k;
             arr.push_back(k);
         }
-        else
+        std::sort(begin(arr), end(arr));
+        for (int j = 0; j < m; j++)
         {
-            int f = 0;
-            for (int j = 0; j < arr.size(); j++)
-            {
-                if (k == arr[j])
-                {
-                    f++;
-                }
-            }
-            if (f == 0)
-            {
-                arr.push_back(k);
-            }
+            std::cout << arr[j] << " ";
         }
+        arr.clear();
+        std::cout << "\n";
     }
-    std::cout << arr.size();
-
     return 0;
 }
+
+// #include <iostream>
+
+// int main()
+// {
+//     int n, s = 0;
+//     std::cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             if (i == n - 1 - j)
+//             {
+//                 std::cout << 1;
+//             }
+//             else
+//             {
+//                 if (i > n - 1 - j)
+//                 {
+//                     std::cout << 2;
+//                 }
+//                 else
+//                 {
+//                     std::cout << 0;
+//                 }
+//             }
+//         }
+//         std::cout << "\n";
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <vector>
+
+// int main()
+// {
+//     int n;
+//     std::cin >> n;
+//     std::vector<int> arr;
+//     for (int i = 0; i < n; i++)
+//     {
+//         int k;
+//         std::cin >> k;
+//         if (arr.size() == 0)
+//         {
+//             arr.push_back(k);
+//         }
+//         else
+//         {
+//             int f = 0;
+//             for (int j = 0; j < arr.size(); j++)
+//             {
+//                 if (k == arr[j])
+//                 {
+//                     f++;
+//                 }
+//             }
+//             if (f == 0)
+//             {
+//                 arr.push_back(k);
+//             }
+//         }
+//     }
+//     std::cout << arr.size();
+
+//     return 0;
+// }
--- lessons.c
+++ lessons.c
@@ -2,6 +2,61 @@
 
 int main(void)
 {
-    printf("Hello, world\n");
+    unsigned char h = 12, m = 3, s = 9;
+
+    printf("%#04x:%#04x:%#04x", h, m, s);
+
     return 0;
 }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int a1 = -123, a2 = 6, a3 = 1024;
+
+//     printf("[%5d]\n[%5d]\n[%5d]\n", a1, a2, a3);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char byte = 65;
+//     unsigned short height = 1055;
+//     int distance = 1000000;
+//     float pi = 3.1415f;
+//     printf("%c\n%d\n%d\n%f\n", byte, height, distance, pi);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+// char ch = 'u';
+// short sh = -55;
+// int var_i = 1024;
+// double var_d = 3.1415;
+
+// printf("%c %d %d %f\n", ch, sh, var_i, var_d);
+
+//     unsigned height = 12345012;
+//     long long dist = 1234567890LL;
+//     long double weight = 45.7845;
+
+//     printf("%u %lld %.4Lf\n", height, dist, weight);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     printf("Hello, world\n");
+//     return 0;
+// }
