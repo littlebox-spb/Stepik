f25d433a73d45a15c28e6e40668aa5e0e8121f37
Alexandr Altukhov <sashae@littlebox.spb.ru>
2024-04-16 11:29:39 +0300

    new file:   .Dict.py.~3158 	new file:   .encrypted.bin.~4899 	new file:   .loremm.txt.~5eb5 	new file:   .passwords.txt.~65aa 	modified:   lessons.c 	modified:   lessons.exe

--- lessons.c
+++ lessons.c
@@ -1,14 +1,24 @@
+#include <cmath>
 #include <stdio.h>
 
 int main(void)
 {
-    long long var_ll = 0;
-    long double var_ld = 0.;
-    short sh = 0;
-    scanf("%lld %f %hd", &var_ll, &var_ld, &sh);
-    printf("%lld %hd", var_ll, sh);
+    printf("%.2f", 6 * std::pow(1 / 3, 2) - 15 * (1 / 3));
+
     return 0;
 }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     long long var_ll = 0;
+//     long double var_ld = 0.;
+//     short sh = 0;
+//     scanf("%lld %f %hd", &var_ll, &var_ld, &sh);
+//     printf("%lld %hd", var_ll, sh);
+//     return 0;
+// }
 
 // #include <stdio.h>
 
