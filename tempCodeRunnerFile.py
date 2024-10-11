bb0fba56daca79cfd0d926f675e44332e9322a1f
littlebox-spb <altukhov.alexandr@mail.ru>
2024-10-11 20:55:08 +0300

    new file:   tempCodeRunnerFile.py 	new file:   theory.py

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+    # for i in range(len(lst)):
+    #     if shift > 0:
+    #         print(lst[i - shift])
+    #     else:
+    #         print(lst[i + shift])
---
+++ theory.py
@@ -0,0 +1,5 @@
+def my_func(a, /, b):
+    print(f"{a=}, {b=}")
+
+
+my_func(a=10, 20)
