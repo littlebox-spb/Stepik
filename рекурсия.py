c5b669e72e29b1378cc6ba2a87834e3113035a6c
Alexander Altukhov <sasha@littlebox.spb.ru>
2024-10-15 11:07:08 +0300

    modified:   "\321\200\320\265\320\272\321\203\321\200\321\201\320\270\321\217.py"

--- рекурсия.py
+++ рекурсия.py
@@ -1,9 +1,21 @@
-def my_pow(x, y):
-    if y == 0:
-        return 1
-    else:
-        return x * my_pow(x, y - 1)
+# def my_pow(x, y):
+#     if y == 0:
+#         return 1
+#     else:
+#         return x * my_pow(x, y - 1)
 
 
-x, y = map(int, input().split())
-print(my_pow(x, y))
+# x, y = map(int, input().split())
+# print(my_pow(x, y))
+
+N = 7
+
+
+def fib_rec(N, f=[1, 1]):
+    if N > 2:
+        f.append(f[-1] + f[-2])
+        fib_rec(N - 1, f)
+    return f
+
+
+print(*fib_rec(N))
